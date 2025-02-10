440cb140a9366094b8e4d8d6191b17977b89ec6d
jotr5402 <jotr5402@colorado.edu>
2025-02-10 13:29:50 -0700

    add initial x

--- loop.m
+++ loop.m
@@ -1,3 +1,3 @@
-function G_N_loop = calculateLoopGs(R, x, h_0, sign)
-    G_N_loop = (2 * ( h_0 - sign * sqrt(R^2 - x.^2) ) ./ R) - sin( sign * acos(x ./ R) );
+function G_N_loop = calculateLoopGs(R, x, x_0, h_0, sign)
+    G_N_loop = (2 * ( h_0 - sign * sqrt(R^2 - (x - x_0).^2) ) ./ R) - sin( sign * acos((x - x_0) ./ R) );
 end
