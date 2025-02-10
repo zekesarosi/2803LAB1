f73e1cb4698a2326ac2cda0dedee32ce441e2234
jotr5402 <jotr5402@colorado.edu>
2025-02-10 10:32:29 -0700

    simplify expression

--- parabola.m
+++ parabola.m
@@ -1,3 +1,3 @@
 function G_N_parabola = calculateParabolaGs(a, b, c, x, h_0)
-    G_N_parabola = (4 * a * (h_0 - (a .* x.^2 + b .* x + c) ) ) ./ ( (4 * a^2 .* x.^2) + (4 * a * b .* x) + b^2 + 1).^1.5 + ( 1 ./ (sqrt( (2 * a .* x).^2 + 1 )));
+    G_N_parabola = ( 4 * a * (h_0 - c) + b^2 + 1 ) ./ ( (4 * a^2 .* x.^2) + (4 * a * b .* x) + b^2 + 1).^1.5;
 end
